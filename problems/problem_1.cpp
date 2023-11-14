813f32d0620261ad7a146c9d45e70deee9cf49a4
rosielady <147365177+rosielady@users.noreply.github.com>
2023-11-14 18:07:49 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,18 +1,21 @@
 float problemSolution1(float consumed_water) {
     float cost;
     if (consumed_water<=30){
-        cost=(13+consumed_water*0.4)
+        cost=(13+consumed_water*0.4);
             break;
     }
     if (consumed_water>30){
         if ((consumed_water-30)<=20) {
             cost=(13+30*0.4+(consumed_water-30)*0.12);}
-        if ((consumed_water)-50<=10){
+        else if ((consumed_water)-50<=10){
             cost=(13+30*0.4+20*0.12+(consumed_water-50)*1.4);}
-        else:
-        cost=(13+30*0.4+20*0.12+10*1.4+(consumed_water-60)*1.5);
+        else {
+        cost=(13+30*0.4+20*0.12+10*1.4+(consumed_water-60)*1.5);}
         
      
+    }
+    if (consumed_water==0){
+        cost=0;
     }
     return cost;
 }
