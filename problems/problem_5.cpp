b5dd658adf33695c7768e0573a2b42d96c8f8fea
rosielady <147365177+rosielady@users.noreply.github.com>
2023-11-14 17:48:08 +0500

    Update problem_5.cpp

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,6 +1,21 @@
 float problemSolution5(float x, float y, char operation) {
    float result;
-   // write your code here
+   switch(operation){
+      case '+':
+         result=(x+y);
+         break;
+      case '*':
+         result=(x*y);
+         break;
+      case '-':
+         result=(x-y);
+         break;
+      case '/':
+         result=(x/y);
+         break;
+      default:
+         result="Not found operation!"
+   }
 
    return result;
 }
