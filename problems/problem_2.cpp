45305431cc05dfe2b2bfcb35c283c85e3795c7aa
rosielady <147365177+rosielady@users.noreply.github.com>
2023-11-14 18:10:02 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,5 +1,5 @@
 float problemSolution2(float i, float j, float k) {
     float result;
-    result=((i>j)?(i>k ? i:k)(j>k ? j:k));
+    result=((i>j)?(i>k ? i:k):(j>k ? j:k));
     return result;
 }
