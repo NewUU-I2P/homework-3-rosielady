eafe212a1565202784ab66e88ecebe346f4af866
rosielady <147365177+rosielady@users.noreply.github.com>
2023-11-14 16:29:45 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,8 +1,5 @@
 float problemSolution2(float i, float j, float k) {
     float result;
-    cout<<"Enter the numbers:";
-    cin>>i>>j>>k;
     result=(i>j)?(i>k ? i:k)(j>k ? j:k);
-
     return result;
 }
