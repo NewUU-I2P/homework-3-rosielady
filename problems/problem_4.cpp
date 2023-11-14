626d4e9cb0e23667a717ac6fb462d1b0479c09ca
rosielady <147365177+rosielady@users.noreply.github.com>
2023-11-14 17:40:43 +0500

    Update problem_4.cpp

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -1,9 +1,8 @@
 #include <string>
 #include <sstream>
 
-std::string problemSolution4(const std::string &macAddress)
-
-{
+std::string problemSolution4(const std::string &macAddress){
+std::string result;
     // write your code here
 
     // make use of control flow statements
